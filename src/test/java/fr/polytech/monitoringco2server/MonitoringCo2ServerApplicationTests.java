6e6ef038fb2c4a3b6642c3ccc13c85807e514dfc
Nicolas Stein <nicolas.stein@etu.sorbonne-universite.fr>
2023-01-30 11:07:07 +0100

    Fix test context load

--- src/test/java/fr/polytech/monitoringco2server/MonitoringCo2ServerApplicationTests.java
+++ src/test/java/fr/polytech/monitoringco2server/MonitoringCo2ServerApplicationTests.java
@@ -2,8 +2,10 @@ package fr.polytech.monitoringco2server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
 class MonitoringCo2ServerApplicationTests {
 
 	@Test
